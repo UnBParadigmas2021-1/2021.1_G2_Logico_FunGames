752f4d85944a8e04fdbc8421f0ccf380e41c6c70
Fellipe <araujo.fellipe@hotmail.com>
2021-09-11 17:40:14 -0300

    #5 Updated game menu.

--- menu.pl
+++ menu.pl
@@ -1,12 +1,14 @@
 menu :- repeat, nl, nl,
         write(' *** Selecione o jogo ***'), nl, nl,
         write('1 - Torre de Hanoi'), nl,
-        write('2 - Macaco & banana'), nl,
+        write('2 - Macaco & Banana'), nl,
+        write('3 - Jogo da Velha'), nl,
         write('0 - Sair'), nl,
         read(Option),
         executar(Option).
 
 executar(Option) :- 
                     Option == 1, [torreHanoi];
-                    Option == 2, write('Você escolheu macaco & banana'), menu;
+                    Option == 2, write('Você escolheu Macaco & Banana'), menu;
+                    Option == 3, write('Você escolheu Jogo da Velha'), menu;
                     Option == 0, true.
