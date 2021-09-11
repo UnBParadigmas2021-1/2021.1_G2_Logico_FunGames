929b7631b487eb37193ee031f2bb7e9e7417ffea
Fellipe <araujo.fellipe@hotmail.com>
2021-09-11 16:55:14 -0300

    #5 Added translate from an integer description of x's move to a board transformation.
    Co-authored-by: Damaso <junio.pereirab@gmail.com>
    Co-authored-by: Calebe <calebersmendes@gmail.com>

--- jogodavelha.pl
+++ jogodavelha.pl
@@ -12,3 +12,24 @@ colwin(Board, Player) :- Board = [_,_,Player,_,_,Player,_,_,Player].
 
 diagwin(Board, Player) :- Board = [Player,_,_,_,Player,_,_,_,Player].
 diagwin(Board, Player) :- Board = [_,_,Player,_,Player,_,Player,_,_].
+
+move([b,B,C,D,E,F,G,H,I], Player, [Player,B,C,D,E,F,G,H,I]).
+move([A,b,C,D,E,F,G,H,I], Player, [A,Player,C,D,E,F,G,H,I]).
+move([A,B,b,D,E,F,G,H,I], Player, [A,B,Player,D,E,F,G,H,I]).
+move([A,B,C,b,E,F,G,H,I], Player, [A,B,C,Player,E,F,G,H,I]).
+move([A,B,C,D,b,F,G,H,I], Player, [A,B,C,D,Player,F,G,H,I]).
+move([A,B,C,D,E,b,G,H,I], Player, [A,B,C,D,E,Player,G,H,I]).
+move([A,B,C,D,E,F,b,H,I], Player, [A,B,C,D,E,F,Player,H,I]).
+move([A,B,C,D,E,F,G,b,I], Player, [A,B,C,D,E,F,G,Player,I]).
+move([A,B,C,D,E,F,G,H,b], Player, [A,B,C,D,E,F,G,H,Player]).
+
+xmove([b,B,C,D,E,F,G,H,I], 1, [x,B,C,D,E,F,G,H,I]).
+xmove([A,b,C,D,E,F,G,H,I], 2, [A,x,C,D,E,F,G,H,I]).
+xmove([A,B,b,D,E,F,G,H,I], 3, [A,B,x,D,E,F,G,H,I]).
+xmove([A,B,C,b,E,F,G,H,I], 4, [A,B,C,x,E,F,G,H,I]).
+xmove([A,B,C,D,b,F,G,H,I], 5, [A,B,C,D,x,F,G,H,I]).
+xmove([A,B,C,D,E,b,G,H,I], 6, [A,B,C,D,E,x,G,H,I]).
+xmove([A,B,C,D,E,F,b,H,I], 7, [A,B,C,D,E,F,x,H,I]).
+xmove([A,B,C,D,E,F,G,b,I], 8, [A,B,C,D,E,F,G,x,I]).
+xmove([A,B,C,D,E,F,G,H,b], 9, [A,B,C,D,E,F,G,H,x]).
+xmove(Board, _, Board) :- write('Jogada inválida, tente novamente!'), nl.
