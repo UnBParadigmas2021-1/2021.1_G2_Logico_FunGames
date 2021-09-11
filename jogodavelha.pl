09859252e8d0c3669cef690017d6234818e47853
Fellipe <araujo.fellipe@hotmail.com>
2021-09-11 16:20:46 -0300

    #5 Added predicates that define the winning conditions.
    Co-authored-by: João Guedes <isudjoao@gmail.com>
    Co-authored-by: Emily Dias <diassousa@yahoo.com.br>

---
+++ jogodavelha.pl
@@ -0,0 +1,14 @@
+win(Board, Player) :- rowwin(Board, Player).
+win(Board, Player) :- colwin(Board, Player).
+win(Board, Player) :- diagwin(Board, Player).
+
+rowwin(Board, Player) :- Board = [Player,Player,Player,_,_,_,_,_,_].
+rowwin(Board, Player) :- Board = [_,_,_,Player,Player,Player,_,_,_].
+rowwin(Board, Player) :- Board = [_,_,_,_,_,_,Player,Player,Player].
+
+colwin(Board, Player) :- Board = [Player,_,_,Player,_,_,Player,_,_].
+colwin(Board, Player) :- Board = [_,Player,_,_,Player,_,_,Player,_].
+colwin(Board, Player) :- Board = [_,_,Player,_,_,Player,_,_,Player].
+
+diagwin(Board, Player) :- Board = [Player,_,_,_,Player,_,_,_,Player].
+diagwin(Board, Player) :- Board = [_,_,Player,_,Player,_,Player,_,_].
