85cf0156f5684c0ce676346feaad3a9014e0e312
sudjoao <isudjoao@gmail.com>
2021-09-12 22:22:50 -0300

    #6 - Add game to the menu

--- menu.pl
+++ menu.pl
@@ -3,6 +3,7 @@ menu :- repeat, nl, nl,
         write('1 - Torre de Hanoi'), nl,
         write('2 - Macaco & Banana'), nl,
         write('3 - Jogo da Velha'), nl,
+        write('4 - Akinator Vingadores'), nl,
         write('0 - Sair'), nl,
         read(Option),
         executar(Option).
@@ -11,4 +12,5 @@ executar(Option) :-
                     Option == 1, [torreHanoi];
                     Option == 2, [macaco];
                     Option == 3, [jogodavelha];
+                    Option == 4, [vingadores];
                     Option == 0, true.
