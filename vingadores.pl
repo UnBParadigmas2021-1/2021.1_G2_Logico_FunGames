1ae878b43535a3f313f301d8fff43835671f4058
sudjoao <isudjoao@gmail.com>
2021-09-12 22:17:24 -0300

    6 - Add Avengers List
    Co-authored-by: Emily Dias <diassousa@yahoo.com.br>

---
+++ vingadores.pl
@@ -0,0 +1,11 @@
+% Avengers List
+% Name, isMan, isDead, hasSuperPowers, isRich, isHuman and hasBlackHair
+avenger('Capitão América', s, n, s, n, s, n).
+avenger('Hulk', s, n, s, n, s, s).
+avenger('Homem de Ferro', s, s, n, s, s, n).
+avenger('Mercúrio', s, s, s, n, s, n).
+avenger('Visão', s, s, s, n, n, n).
+avenger('Thor', s, n, s, n, n, n).
+avenger('Gavião Arqueiro', s, n, n, s, s, n).
+avenger('Feiticeira Escarlate', n, n, s, n, s, n).
+avenger('Viúva Negra', n, s, n, n, s, n).
